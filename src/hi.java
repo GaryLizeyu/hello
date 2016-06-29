c92e06ebcb15201df7b66591069bbe6cdba772d9
Zeyu <zl2206@outlook.com>
2016-06-29 19:38:39 -0230

    public class hi {
    	public static void main(String[] args) {
    		// TODO Auto-generated method
                    System.out.println("Hi,2");
                    System.out.println("Hi,2");
    	}

--- src/hi.java
+++ src/hi.java
@@ -3,6 +3,8 @@ public class hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+               System.out.println("Hi,2016-6-29 15:26:58");
+               System.out.println("Hi,2");
 	}
 
 }
